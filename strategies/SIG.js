06f87e6bdd147b0af0fd3f7a4d8a0ef44e0c5c4c
Josh Chu <joshchu00@gmail.com>
2018-06-12 14:11:39 +0800

    Update strategy SIG & config

--- strategies/SIG.js
+++ strategies/SIG.js
@@ -1,13 +1,38 @@
 var method = {};
 
 method.init = function() {
+  this.name = 'Triple Moving Average';
+  this.requiredHistory = this.settings.long;
+
+  this.addIndicator('short', 'SMA', this.settings.short)
+  this.addIndicator('medium', 'SMA', this.settings.medium)
+  this.addIndicator('long', 'SMA', this.settings.long)
+
+  this.prevAdvice;
 }
 
 method.update = function(candle) {
+  this.indicators.short.update(candle.close)
+  this.indicators.medium.update(candle.close)
+  this.indicators.long.update(candle.close)
 }
 
 method.check = function() {
-  this.advice('long')
+  const short = this.indicators.short.result;
+  const medium = this.indicators.medium.result;
+  const long = this.indicators.long.result;
+
+  advice = 'short'
+
+  if((short > medium) && (medium > long)) {
+    advice = 'long'
+  }
+
+  if (advice === this.prevAdvice) {
+    advice = 'same'
+  }
+  
+  this.advice(advice)
 }
 
 module.exports = method;
